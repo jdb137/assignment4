95354c06c57797d788f41058d116c9b38967a817
jdb137 <jdb137@pitt.edu>
2017-04-12 18:51:26 -0400

    Final
    Yay!

--- assignment4.js
+++ assignment4.js
@@ -14,4 +14,30 @@
 (function() {
   // Magic!
   console.log('Keepin\'n it clean with an external script!');
+  $.ajax({
+    type: 'GET',
+    url: "http://www.mattbowytz.com/simple_api.json?data=all",
+    dataType: 'json',
+    success: function(thedata){
+      $(".flexsearch-input").keyup(function(e){
+        $(".suggestions").remove();
+        var word = $(".flexsearch-input").val().toLowerCase();
+        var length = word.length;
+        var myarray = new Array();
+
+        $.each(thedata.data, function(index1,value1){
+          $.each(value1, function(index2,value2){
+            var suggestion = value2.substring(0,length).toLowerCase();
+            if(suggestion===word&&$(".flexsearch-input").val().length!==0){
+              myarray.push(value2);
+            }
+          });
+        });
+        $.each(myarray, function(index, value){
+          $("#mainForm").append('<div class="suggestions"><p>'+ value +'</p></div>');
+        });
+        console.log(word);
+      });
+    }
+  });
 })();
